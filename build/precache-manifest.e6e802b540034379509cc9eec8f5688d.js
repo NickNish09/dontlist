ba008bcd49b82755c293e260ebf578a2e066fc08
nicknish09 <nnmarques97@gmail.com>
2019-08-24 15:27:29 -0300

    firebase fufnado

---
+++ build/precache-manifest.e6e802b540034379509cc9eec8f5688d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19da0ccd7dddea0587fb5e43a59e5dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fecd35ceead4bade2fe",
+    "url": "/static/css/main.6a138fd2.chunk.css"
+  },
+  {
+    "revision": "1fe1c98d70ea0a322043",
+    "url": "/static/js/2.b42613bb.chunk.js"
+  },
+  {
+    "revision": "8fecd35ceead4bade2fe",
+    "url": "/static/js/main.236da00b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
